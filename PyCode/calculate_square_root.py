eaca530db449566faaa8514b4c0a89178c66280e
RubyYan888 <ruihong666@outlook.com>
2024-06-19 10:49:02 +0800

    Create calculate_square_root.py
    计算一个数的平方根

---
+++ PyCode/calculate_square_root.py
@@ -0,0 +1,13 @@
+# 计算一个数的平方根
+import math
+
+def calculate_square_root(num):
+    return math.sqrt(num)
+
+def main():
+    number = 16
+    sqrt = calculate_square_root(number)
+    print(f"The square root of {number} is: {sqrt}")
+
+if __name__ == "__main__":
+    main()
