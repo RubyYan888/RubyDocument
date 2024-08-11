0b125c01660e825d5086dac8cbc525871af97e24
RubyYan888 <ruihong666@outlook.com>
2024-08-11 14:51:56 +0800

    Create func_py_convert_fahrenheit_to_kelvin.py

---
+++ PyCode/func_py_convert_fahrenheit_to_kelvin.py
@@ -0,0 +1,5 @@
+# func_py_convert_fahrenheit_to_kelvin.py
+def func_py_convert_fahrenheit_to_kelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5/9
+
+print(func_py_convert_fahrenheit_to_kelvin(32))
