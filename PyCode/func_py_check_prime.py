0c998d77b7fefd08d62d386c42487e342f65c3ad
RubyYan888 <ruihong666@outlook.com>
2024-09-21 09:26:20 +0800

    Create func_py_check_prime.py

---
+++ PyCode/func_py_check_prime.py
@@ -0,0 +1,10 @@
+# func_py_check_prime.py
+def func_py_check_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(func_py_check_prime(11))
